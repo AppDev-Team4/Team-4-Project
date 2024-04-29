14b50916267e29c85b1b89f46a04bdff4a6bdfde
AppDev-Team4 <168151311+AppDev-Team4@users.noreply.github.com>
2024-04-29 13:48:24 -0500

    Update itemQuery.php

--- pages/itemQuery.php
+++ pages/itemQuery.php
@@ -9,7 +9,6 @@ if (isset($_SESSION['customerID'])) {
 } else {
 	$customerID = NULL;
 }
-
 /** 
 * @brief getItems
 *
@@ -44,20 +43,19 @@ function getItems($limit, $currentPage, $category, $sortOrder, $filterByPrice, $
 		$id = strval($customerID);
 		$sql = "SELECT COUNT(*) FROM customerpurchases WHERE CustomerID = ";
 		$sql .= $id;
-		echo $sql;
+		
 		$result = $conn->query($sql);
 		if (!$result) {
 			die("Query failed: " . $conn->error);
 		}	
-		//$numrows = mysql_num_rows($result);
+		$numrows = $result->fetch_assoc();
 		
 		if ($numrows != 0) {
 			// pass the current session customerID into python fuction to get recommended products for this user.
-			$output = shell_exec('/pages/productQuery.py' . $customerID);
-			
+			$command = "python productQuery.py " . $customerID;
+			$output = shell_exec($command);
 			//Build item query for the recommended items for this user
-			$sql = "SELECT * FROM Products WHERE ProductID = ";
-			$sql .= $output;
+			$sql = "SELECT * FROM Products WHERE ProductID in ($output)";
 		}
 		
 	} else {
@@ -156,69 +154,120 @@ function getItems($limit, $currentPage, $category, $sortOrder, $filterByPrice, $
 * @returns The total number of items based on limiting
 * condtion of SQL query.
 */
-function getTotalItems($category, $filterByPrice, $search) {
+function getTotalItems($category, $filterByPrice, $search, $customerID, $sortOrder) {
     global $conn;
 
     $sql = "SELECT COUNT(*) as totalItems FROM Products";
+	// if the Personalized Recommendation button is clicked it will query items based off AI Model
+	if ($sortOrder == 'personalized_recommendation' and $customerID != NULL){
+		// check if customer has previous purchase history by checking 
+		$id = strval($customerID);
+		$sql = "SELECT COUNT(*) FROM customerpurchases WHERE CustomerID = ";
+		$sql .= $id;
+		
+		$result = $conn->query($sql);
+		if (!$result) {
+			die("Query failed: " . $conn->error);
+		}	
+		$numrows = $result->fetch_assoc();
+		
+		if ($numrows != 0) {
+			// Personalized recommendation only suggests 8 products
+			$totalItems = 8;
+		}
+			
+	} else {
+	   if ($category != '') {
+			$sql .= " WHERE";
+			switch ($category) {
+				case 'clothing':
+					$sql .= " Category = 'Clothing'";
+					break;
+				case 'homeGoods':
+					$sql .= " Category = 'Home Goods'";
+					break;
+				case 'hobbies':
+					$sql .= " Category = 'Hobbies'";
+					break;
+				case 'healthWellness':
+					$sql .= " Category = 'Wellness'";
+					break;
+				default:
+					break;
+			}
+		}
+		if($filterByPrice != '' and $category == ''){
+			$sql .= ' WHERE';
+		} else if($filterByPrice != '' and $category != ''){
+			$sql .= ' AND';
+		}
+		if($filterByPrice != ''){
+			switch($filterByPrice){
+				case '0-24.99':
+					$sql .= " Price <= 24.99";
+					break;
+				case '25-49.99':
+					$sql .= " Price >= 25 AND Price <= 49.99";
+					break;
+				case '50-99.99':
+					$sql .= " Price >= 50 AND Price <= 99.99";
+					break;
+				case '100':
+					$sql .= " Price >= 100";
+					break;
+				default:
+					break;
+			}
+		}
 
-   if ($category != '') {
-        $sql .= " WHERE";
-        switch ($category) {
-            case 'clothing':
-                $sql .= " Category = 'Clothing'";
-                break;
-            case 'homeGoods':
-                $sql .= " Category = 'Home Goods'";
-                break;
-            case 'hobbies':
-                $sql .= " Category = 'Hobbies'";
-                break;
-            case 'healthWellness':
-                $sql .= " Category = 'Wellness'";
-                break;
-            default:
-                break;
-        }
-    }
-    if($filterByPrice != '' and $category == ''){
-        $sql .= ' WHERE';
-    } else if($filterByPrice != '' and $category != ''){
-        $sql .= ' AND';
-    }
-    if($filterByPrice != ''){
-        switch($filterByPrice){
-            case '0-24.99':
-                $sql .= " Price <= 24.99";
-                break;
-            case '25-49.99':
-                $sql .= " Price >= 25 AND Price <= 49.99";
-                break;
-            case '50-99.99':
-                $sql .= " Price >= 50 AND Price <= 99.99";
-                break;
-            case '100':
-                $sql .= " Price >= 100";
-                break;
-            default:
-                break;
-        }
-    }
-
-    if ($search != '') {
-        if ($category != '' || $filterByPrice != '') {
-            $sql .= " AND";
-        } else {
-            $sql .= " WHERE";
-        }
-        $sql .= " Name LIKE '%$search%'";
-    }
+		if ($search != '') {
+			if ($category != '' || $filterByPrice != '') {
+				$sql .= " AND";
+			} else {
+				$sql .= " WHERE";
+			}
+			$sql .= " Name LIKE '%$search%'";
+		}
 
     $totalItemsQuery = $conn->query($sql);
     $totalItemsResult = $totalItemsQuery->fetch_assoc();
     $totalItems = $totalItemsResult['totalItems'];
+	}
 
     return $totalItems;
 }
+/**
+* @brief previousCustHistory
+*
+* Finds if current account logged into website has previous purchase history.
+* Used to determine if account has access to personalized recommendation 
+* feature
+*
+* @para customerID: customerID of the current account in session on website
+*
+* @returns True if current account has previous purchase and false if it doesn't.
+*
+*/
+function previousCustHistory($customerID){
+	global $conn;
+	
+	$id = strval($customerID);
+	$sql = "SELECT COUNT(*) FROM customerpurchases WHERE customerID = ";
+	$sql .= $id;
+	$History = $conn->query($sql);
+    $HistoryResult = $History->fetch_assoc();
+	
+	if ($HistoryResult >= 1){
+		//user that's logged in has previous purchase history
+		$validHistory = True;
+	} else {
+		//user doesn't have previous purchase history
+		$validHistory = False;
+	}
+	return $validHistory;
+}
+
+	
 
 // All the basic values that will be used to add items to catalog page
 $itemsPerPage = 12;
@@ -228,6 +277,6 @@ $sortOrder = isset($_GET['sort_by']) ? $_GET['sort_by'] : 'featured';
 $filterByPrice = isset($_GET['filterByPrice']) ? $_GET['filterByPrice'] : '';
 
 $result = getItems($itemsPerPage, $currentPage, $category, $sortOrder, $filterByPrice, $search, $customerID);
-$totalItems = getTotalItems($category, $filterByPrice, $search);
+$totalItems = getTotalItems($category, $filterByPrice, $search, $customerID, $sortOrder);
 $totalPages = ceil($totalItems / $itemsPerPage);
 ?>
